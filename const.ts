ce338215238e5717a564d7db4c401bcad72dc81f
shaulov <shineginn@gmail.com>
2024-03-21 17:33:38 +0300

    Add file to store constants and add languages to it

---
+++ const.ts
@@ -0,0 +1,7 @@
+export const LANGUAGES = [
+  { id: 'croatian', name: 'Croatian', src: '/images/flags/hr.svg' },
+  { id: 'spanish', name: 'Spanish', src: '/images/flags/es.svg' },
+  { id: 'french', name: 'French', src: '/images/flags/fr.svg' },
+  { id: 'italian', name: 'Italian', src: '/images/flags/it.svg' },
+  { id: 'japanese', name: 'Japanese', src: '/images/flags/jp.svg' },
+];
