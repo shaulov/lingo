16cfceda1ec0b9202f530107222b5cfdb21b0116
shaulov <shineginn@gmail.com>
2025-02-03 23:22:47 +0300

    feat: add api route for stripe webhook

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,59 @@
+import {headers} from "next/headers";
+import {NextResponse} from "next/server";
+import {eq} from "drizzle-orm";
+import Stripe from "stripe";
+import {stripe} from "@/lib/stripe";
+import db from "@/db/drizzle";
+import {userSubscription} from "@/db/schema";
+
+export async function POST(request: Request) {
+    const body = await request.text();
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!,
+        );
+    } catch(error: any) {
+        return new NextResponse(`Webhook error: ${error?.message}`, {
+            status: 400,
+        });
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    if (event.type === "checkout.session.completed") {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        );
+
+        if (!session?.metadata?.userId) {
+            return new NextResponse("User ID is required", { status: 400 });
+        }
+
+        await db.insert(userSubscription).values({
+            userId: session.metadata.userId,
+            stripeCustomerId: subscription.customer as string,
+            stripeSubscriptionId: subscription.id,
+            stripePriceId: subscription.items.data[0].price.id,
+            stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        });
+    }
+
+    if (event.type === "invoice.payment_succeeded") {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string,
+        );
+
+        await db.update(userSubscription).set({
+            stripePriceId: subscription.items.data[0].price.id,
+            stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        }).where(eq(userSubscription.stripeSubscriptionId, subscription.id));
+    }
+
+    return new NextResponse(null, { status: 200 });
+}
